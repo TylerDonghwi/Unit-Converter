863de79c5a4777cf6d45204367842faa2d3a5a33
TylerDonghwi <tylerdonghwikim@email.com>
2022-05-17 14:09:15 +1200

    basic input without conversion

--- weight-converter/weight-convert.js
+++ weight-converter/weight-convert.js
@@ -1,5 +1,28 @@
-kilograms = document.getElementById("kilograms")
-pounds = document.getElementById("pounds")
-ounce = document.getElementById("ounce")
+const gramsInPound = 453.59237;
+const gramsInOunce = 28.349523125;
 
-console.log(kilograms)
+document.getElementById("submitBtn").onclick = function() {
+    let grams = document.getElementById("grams").checked
+    let pounds = document.getElementById("pounds").checked
+    let ounce = document.getElementById("ounce").checked
+    let input = document.getElementById("input").value
+
+    if (grams) {
+        document.getElementById("gramOutput").innerHTML += input;
+    } else if (pounds) {
+        document.getElementById("poundOutput").innerHTML += input;
+    } else if (ounce) {
+        document.getElementById("ozOutput").innerHTML += input;
+    } else {
+        alert("Please choose what unit you're converting from")
+    }
+}
+
+// two functions returns any values into kg
+function fromPoundsToKg(input) {
+    return gramsInPound * input;
+}
+
+function fromOunceToKg(input) {
+    return gramsInOunce * input;
+}
